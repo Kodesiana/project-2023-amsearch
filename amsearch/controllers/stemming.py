9c80b0984833b8555d5826f713223289639e0f08
fahminlb33 <fahmi@kodesiana.com>
2024-09-06 10:57:53 +0700

    chore: format

--- amsearch/controllers/stemming.py
+++ amsearch/controllers/stemming.py
@@ -105,30 +105,42 @@ def stem():
         stats_ug18=stats_description(stem_stats, "ug18", len(stems)),
     )
 
+
 @router.route("/stemming_word", methods=["GET", "POST"])
 def stem_word():
     # show page
     if request.method == "GET":
         return render_template(
-            "pages/public/stemming_single.html", input_word="", stemmed_word="", alternatives=[]
+            "pages/public/stemming_single.html",
+            input_word="",
+            stemmed_word="",
+            alternatives=[],
         )
 
     # get text
     input_word = request.form.get("input-word")
     if not input_word:
         return render_template(
-            "pages/public/stemming_single.html", input_word="", stemmed_word="", alternatives=[]
+            "pages/public/stemming_single.html",
+            input_word="",
+            stemmed_word="",
+            alternatives=[],
         )
-    
+
     # stem the word
     stemmed_word = VectorSearchInstance.stemmer.stem_ams(input_word.lower())
 
     # find alternative if the word is not in the dictionary
     alternatives = []
     if stemmed_word not in VectorSearchInstance.stemmer.kamus:
-        alternatives = process.extract(stemmed_word, VectorSearchInstance.stemmer.kamus, limit=5)
+        alternatives = process.extract(
+            stemmed_word, VectorSearchInstance.stemmer.kamus, limit=5
+        )
 
     # render page
     return render_template(
-            "pages/public/stemming_single.html", input_word=input_word, stemmed_word=stemmed_word, alternatives=alternatives
-        )
+        "pages/public/stemming_single.html",
+        input_word=input_word,
+        stemmed_word=stemmed_word,
+        alternatives=alternatives,
+    )
