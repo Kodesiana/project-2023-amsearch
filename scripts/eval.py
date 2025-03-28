b3f05b083a01fa27f0505fcd5ed94bc2aa404d1a
fahminlb33 <fahmi@kodesiana.com>
2025-03-28 21:07:35 +0700

    feat: update training & eval scripts

--- scripts/eval.py
+++ scripts/eval.py
@@ -2,14 +2,19 @@ import logging
 import argparse
 
 import bm25s
+import pandas as pd
+
 from beir import LoggingHandler
-from beir.retrieval import models
+from beir.retrieval.models import SentenceBERT
 from beir.datasets.data_loader import GenericDataLoader
 from beir.retrieval.evaluation import EvaluateRetrieval
 from beir.retrieval.search.dense import DenseRetrievalExactSearch
 
-from amsearch.services import Stemmer
+from stemmer import Stemmer
 
+# ----------------------- HELPERS -----------------------
+
+K_VALUES = [1, 3, 5, 10, 100, 1000]
 
 # https://www.kaggle.com/code/xhlulu/benchmark-bm25-on-beir
 def postprocess_results_for_eval(results, scores, query_ids):
@@ -27,74 +32,80 @@ def postprocess_results_for_eval(results, scores, query_ids):
 
     return result_dict_for_eval
 
+def save_results(model_name: str, items: list[dict[str, float]]):
+    metrics = [{"metric": k.split("@")[0], "k": k.split("@")[1], "value": v} for col in items for k, v in col.items()]
+    df_metrics = pd.DataFrame(metrics)
+
+    safe_model_name = model_name.replace("/", "_")
+    df_metrics.to_csv(f"eval-{safe_model_name}.csv", index=None)
+
+# ----------------------- COMMANDS -----------------------
 
 def eval_bm25(args):
     # load dataset
-    corpus, queries, qrels = GenericDataLoader(
-        data_folder=args.dataset_path, qrels_file=f"{args.dataset_path}/qrels.tsv"
-    ).load_custom()
+    corpus, queries, qrels = GenericDataLoader(data_folder=args.dataset_path, qrels_file=f"{args.dataset_path}/qrels.tsv").load_custom()
 
+    # build BM25 corpus
     corpus_ids, corpus_lst = [], []
     for key, val in corpus.items():
         corpus_ids.append(key)
-        corpus_lst.append(val["title"] + " " + val["text"])
+        corpus_lst.append(val["text"])
+        # corpus_lst.append(val["title"] + " " + val["text"])
 
     qids, queries_lst = [], []
     for key, val in queries.items():
         qids.append(key)
         queries_lst.append(val)
 
-    if args.model_name == "bm25-ams":
+    # tokenize data, optionally with tokenization
+    if args.eval_mode == "bm25-ams":
         stemmer = Stemmer(args.vocab_path)
-        corpus_tokens = bm25s.tokenize(
-            corpus_lst, stemmer=lambda lst: list(map(stemmer.stem_ams, lst))
-        )
-        query_tokens = bm25s.tokenize(
-            queries_lst, stemmer=lambda lst: list(map(stemmer.stem_ams, lst))
-        )
+        corpus_tokens = bm25s.tokenize(corpus_lst, stemmer=lambda lst: list(map(stemmer.stem_ams, lst)))
+        query_tokens = bm25s.tokenize(queries_lst, stemmer=lambda lst: list(map(stemmer.stem_ams, lst)))
     else:
         corpus_tokens = bm25s.tokenize(corpus_lst)
         query_tokens = bm25s.tokenize(queries_lst)
 
+    # index data
     model = bm25s.BM25(method="lucene", k1=1.2, b=0.75)
     model.index(corpus_tokens)
 
-    queried_results, queried_scores = model.retrieve(
-        query_tokens, corpus=corpus_ids, k=1000, n_threads=1
-    )
+    # retrieve data
+    queried_results, queried_scores = model.retrieve(query_tokens, corpus=corpus_ids, k=1000, n_threads=1)
     results_dict = postprocess_results_for_eval(queried_results, queried_scores, qids)
 
-    ndcg, _map, recall, precision = EvaluateRetrieval.evaluate(
-        qrels, results_dict, [1, 3, 5, 10, 100, 1000]
-    )
+    # evaluate model with NDCG@k, MAP@K, Recall@K and Precision@K
+    ndcg, _map, recall, precision = EvaluateRetrieval.evaluate(qrels, results_dict, K_VALUES)
+
+    # save metrics
+    save_results(args.model_name, [ndcg, _map, recall, precision])
 
 
 def eval_dense(args):
     # load dataset
-    corpus, queries, qrels = GenericDataLoader(
-        data_folder=args.dataset_path, qrels_file=f"{args.dataset_path}/qrels.tsv"
-    ).load_custom()
+    corpus, queries, qrels = GenericDataLoader(data_folder=args.dataset_path, qrels_file=f"{args.dataset_path}/qrels.tsv").load_custom()
 
     # load the SBERT model
-    model = DenseRetrievalExactSearch(
-        models.SentenceBERT(args.model_name), batch_size=args.batch_size
-    )
+    model = DenseRetrievalExactSearch(SentenceBERT(args.model_name), batch_size=args.batch_size)
 
-    # retrieve
+    # retrieve data
     retriever = EvaluateRetrieval(model, score_function=args.score_function)
     results = retriever.retrieve(corpus, queries)
 
-    # evaluate your model with NDCG@k, MAP@K, Recall@K and Precision@K  where k = [1,3,5,10,100,1000]
-    ndcg, _map, recall, precision = retriever.evaluate(
-        qrels, results, retriever.k_values
-    )
+    # evaluate model with NDCG@k, MAP@K, Recall@K and Precision@K
+    ndcg, _map, recall, precision = retriever.evaluate(qrels, results, K_VALUES)
 
+    # save metrics
+    save_results(args.model_name, [ndcg, _map, recall, precision])
+
+
+# ----------------------- ENTRY POINT -----------------------
 
 def main(args):
-    if args.model_name == "bm25" or args.model_name == "bm25-ams":
-        eval_bm25(args)
-    else:
+    if args.eval_mode == "dense":
         eval_dense(args)
+    else:
+        eval_bm25(args)
 
 
 if __name__ == "__main__":
@@ -108,17 +119,13 @@ if __name__ == "__main__":
 
     # create CLI parser
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model_name", type=str, required=True)
-    parser.add_argument("--dataset_path", type=str, required=True)
+    parser.add_argument("--model-name", type=str, required=True)
+    parser.add_argument("--dataset-path", type=str, required=True)
 
-    parser.add_argument("--batch_size", type=int, default=16)
-    parser.add_argument(
-        "--score_function", type=str, default="dot", choices=["dot", "cos_sim"]
-    )
-    parser.add_argument("--vocab_path", type=str)
+    parser.add_argument("--batch-size", type=int, default=16)
+    parser.add_argument("--eval-mode", type=str, default="dense", choices=["dense", "bm25", "bm25-ams"])
+    parser.add_argument("--vocab-path", type=str, default="data/sundabaru1-vocab.txt")
+    parser.add_argument("--score-function", type=str, default="dot", choices=["dot", "cos_sim"])
 
     args = parser.parse_args()
-    print(args)
-
-    # run app
     main(args)
--- scripts/train.py
+++ scripts/train.py
@@ -3,15 +3,14 @@ import logging
 import argparse
 
 from torch.utils.data import DataLoader, Dataset
+from sentence_transformers.losses import MultipleNegativesRankingLoss
 from sentence_transformers import (
     InputExample,
     LoggingHandler,
     SentenceTransformer,
-    losses,
 )
 
-
-class MSMARCODataset(Dataset):
+class TripletDataset(Dataset):
     def __init__(self, dataset_path: str):
         with open(dataset_path, "r") as f:
             self.dataset = [json.loads(x) for x in f]
@@ -24,20 +23,18 @@ class MSMARCODataset(Dataset):
         return len(self.dataset)
 
 
-# https://github.com/UKPLab/sentence-transformers/blob/master/examples/training/ms_marco/train_bi-encoder_mnrl.py
+# https://github.com/UKPLab/sentence-transformers/blob/master/examples/sentence_transformer/training/ms_marco/train_bi-encoder_mnrl.py
 def main(args):
     # create data loader
-    train_dataset = MSMARCODataset(args.dataset_path)
-    train_dataloader = DataLoader(
-        train_dataset, shuffle=True, batch_size=args.train_batch_size
-    )
+    train_dataset = TripletDataset(args.dataset_path)
+    train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=args.batch_size)
 
     # create model
     model = SentenceTransformer(args.model_name)
     model.max_seq_length = args.max_seq_length
 
     # create loss
-    train_loss = losses.MultipleNegativesRankingLoss(model=model)
+    train_loss = MultipleNegativesRankingLoss(model=model)
 
     # train the model
     model.fit(
@@ -51,7 +48,7 @@ def main(args):
     )
 
     # save the model
-    model.save(args.model_output_path)
+    model.save(args.output_path)
 
 
 if __name__ == "__main__":
@@ -65,14 +62,14 @@ if __name__ == "__main__":
 
     # create parser
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model_name", type=str, required=True)
-    parser.add_argument("--dataset_path", type=str, required=True)
-    parser.add_argument("--model_output_path", type=str, required=True)
+    parser.add_argument("--model-name", type=str, required=True)
+    parser.add_argument("--dataset-path", type=str, required=True)
+    parser.add_argument("--output-path", type=str, required=True)
 
-    parser.add_argument("--train_batch_size", default=64, type=int)
-    parser.add_argument("--max_seq_length", default=300, type=int)
+    parser.add_argument("--batch-size", default=8, type=int)
+    parser.add_argument("--max-seq-length", default=512, type=int)
     parser.add_argument("--epochs", default=10, type=int)
-    parser.add_argument("--warmup_steps", default=1000, type=int)
+    parser.add_argument("--warmup-steps", default=1000, type=int)
     parser.add_argument("--lr", default=2e-5, type=float)
 
     args = parser.parse_args()
