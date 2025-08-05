34d4c1da4f588eb03ff3906711f3517ce9d9b0d8
fahminlb33 <fahminlb33@gmail.com>
2025-08-05 19:08:32 +0700

    feat: update train and eval bootstrap script

--- scripts/run-eval.sh
+++ scripts/run-eval.sh
@@ -2,57 +2,73 @@
 set -euxo pipefail
 
 # --- eval pretrained models
-python eval.py --model-name sentence-transformers/all-mpnet-base-v2 --dataset-path ../data/cleaned 
-python eval.py --model-name sentence-transformers/multi-qa-distilbert-cos-v1 --dataset-path ../data/cleaned 
-python eval.py --model-name sentence-transformers/multi-qa-MiniLM-L6-cos-v1 --dataset-path ../data/cleaned 
-python eval.py --model-name sentence-transformers/msmarco-distilbert-cos-v5 --dataset-path ../data/cleaned 
-python eval.py --model-name sentence-transformers/msmarco-MiniLM-L6-cos-v5 --dataset-path ../data/cleaned 
+python eval.py --model-name sentence-transformers/all-mpnet-base-v2 --dataset-path ../data/dataset-new/cleaned 
+python eval.py --model-name sentence-transformers/multi-qa-distilbert-cos-v1 --dataset-path ../data/dataset-new/cleaned 
+python eval.py --model-name sentence-transformers/multi-qa-MiniLM-L6-cos-v1 --dataset-path ../data/dataset-new/cleaned 
+python eval.py --model-name sentence-transformers/msmarco-distilbert-cos-v5 --dataset-path ../data/dataset-new/cleaned 
+python eval.py --model-name sentence-transformers/msmarco-MiniLM-L6-cos-v5 --dataset-path ../data/dataset-new/cleaned 
+python eval.py --model-name StevenLimcorn/MelayuBERT --dataset-path ../data/dataset-new/cleaned 
+python eval.py --model-name indolem/indobert-base-uncased --dataset-path ../data/dataset-new/cleaned
 
-python eval.py --model-name sentence-transformers/all-mpnet-base-v2 --dataset-path ../data/cleaned --stemming
-python eval.py --model-name sentence-transformers/multi-qa-distilbert-cos-v1 --dataset-path ../data/cleaned --stemming
-python eval.py --model-name sentence-transformers/multi-qa-MiniLM-L6-cos-v1 --dataset-path ../data/cleaned --stemming
-python eval.py --model-name sentence-transformers/msmarco-distilbert-cos-v5 --dataset-path ../data/cleaned --stemming
-python eval.py --model-name sentence-transformers/msmarco-MiniLM-L6-cos-v5 --dataset-path ../data/cleaned --stemming
+python eval.py --model-name sentence-transformers/all-mpnet-base-v2 --dataset-path ../data/dataset-new/cleaned --stemming
+python eval.py --model-name sentence-transformers/multi-qa-distilbert-cos-v1 --dataset-path ../data/dataset-new/cleaned --stemming
+python eval.py --model-name sentence-transformers/multi-qa-MiniLM-L6-cos-v1 --dataset-path ../data/dataset-new/cleaned --stemming
+python eval.py --model-name sentence-transformers/msmarco-distilbert-cos-v5 --dataset-path ../data/dataset-new/cleaned --stemming
+python eval.py --model-name sentence-transformers/msmarco-MiniLM-L6-cos-v5 --dataset-path ../data/dataset-new/cleaned --stemming
+python eval.py --model-name StevenLimcorn/MelayuBERT --dataset-path ../data/dataset-new/cleaned --stemming
+python eval.py --model-name indolem/indobert-base-uncased --dataset-path ../data/dataset-new/cleaned --stemming
 
 # --- eval tuned models
 # non stemming
-python eval.py --model-name ../models/ams-all-mpnet-base-v2 --dataset-path ../data/cleaned
-python eval.py --model-name ../models/ams-multi-qa-distilbert-cos-v1 --dataset-path ../data/cleaned
-python eval.py --model-name ../models/ams-multi-qa-MiniLM-L6-cos-v1 --dataset-path ../data/cleaned
-python eval.py --model-name ../models/ams-msmarco-distilbert-cos-v5 --dataset-path ../data/cleaned
-python eval.py --model-name ../models/ams-msmarco-MiniLM-L6-cos-v5 --dataset-path ../data/cleaned
-python eval.py --model-name ../models/ams-bow.joblib --dataset-path ../data/cleaned 
-python eval.py --model-name ../models/ams-tfidf.joblib --dataset-path ../data/cleaned 
-python eval.py --model-name ../models/ams-doc2vec.gensim --dataset-path ../data/cleaned 
-python eval.py --model-name ../models/ams-fasttext.gensim --dataset-path ../data/cleaned 
+python eval.py --model-name ../models/ams-all-mpnet-base-v2 --dataset-path ../data/dataset-new/cleaned
+python eval.py --model-name ../models/ams-multi-qa-distilbert-cos-v1 --dataset-path ../data/dataset-new/cleaned
+python eval.py --model-name ../models/ams-multi-qa-MiniLM-L6-cos-v1 --dataset-path ../data/dataset-new/cleaned
+python eval.py --model-name ../models/ams-msmarco-distilbert-cos-v5 --dataset-path ../data/dataset-new/cleaned
+python eval.py --model-name ../models/ams-msmarco-MiniLM-L6-cos-v5 --dataset-path ../data/dataset-new/cleaned
+python eval.py --model-name ../models/ams-melayubert --dataset-path ../data/dataset-new/cleaned
+python eval.py --model-name ../models/ams-indobert --dataset-path ../data/dataset-new/cleaned
+python eval.py --model-name ../models/ams-bow.joblib --dataset-path ../data/dataset-new/cleaned 
+python eval.py --model-name ../models/ams-tfidf.joblib --dataset-path ../data/dataset-new/cleaned 
+python eval.py --model-name ../models/ams-doc2vec.gensim --dataset-path ../data/dataset-new/cleaned 
+python eval.py --model-name ../models/ams-fasttext.gensim --dataset-path ../data/dataset-new/cleaned 
 
-python eval.py --model-name ../models/ams-all-mpnet-base-v2 --dataset-path ../data/cleaned --stemming
-python eval.py --model-name ../models/ams-multi-qa-distilbert-cos-v1 --dataset-path ../data/cleaned --stemming
-python eval.py --model-name ../models/ams-multi-qa-MiniLM-L6-cos-v1 --dataset-path ../data/cleaned --stemming
-python eval.py --model-name ../models/ams-msmarco-distilbert-cos-v5 --dataset-path ../data/cleaned --stemming
-python eval.py --model-name ../models/ams-msmarco-MiniLM-L6-cos-v5 --dataset-path ../data/cleaned --stemming
-python eval.py --model-name ../models/ams-bow.joblib --dataset-path ../data/cleaned --stemming
-python eval.py --model-name ../models/ams-tfidf.joblib --dataset-path ../data/cleaned --stemming
-python eval.py --model-name ../models/ams-doc2vec.gensim --dataset-path ../data/cleaned  --stemming
-python eval.py --model-name ../models/ams-fasttext.gensim --dataset-path ../data/cleaned  --stemming
+python eval.py --model-name ../models/ams-all-mpnet-base-v2 --dataset-path ../data/dataset-new/cleaned --stemming
+python eval.py --model-name ../models/ams-multi-qa-distilbert-cos-v1 --dataset-path ../data/dataset-new/cleaned --stemming
+python eval.py --model-name ../models/ams-multi-qa-MiniLM-L6-cos-v1 --dataset-path ../data/dataset-new/cleaned --stemming
+python eval.py --model-name ../models/ams-msmarco-distilbert-cos-v5 --dataset-path ../data/dataset-new/cleaned --stemming
+python eval.py --model-name ../models/ams-msmarco-MiniLM-L6-cos-v5 --dataset-path ../data/dataset-new/cleaned --stemming
+python eval.py --model-name ../models/ams-melayubert --dataset-path ../data/dataset-new/cleaned --stemming
+python eval.py --model-name ../models/ams-indobert --dataset-path ../data/dataset-new/cleaned --stemming
+python eval.py --model-name ../models/ams-bow.joblib --dataset-path ../data/dataset-new/cleaned --stemming
+python eval.py --model-name ../models/ams-tfidf.joblib --dataset-path ../data/dataset-new/cleaned --stemming
+python eval.py --model-name ../models/ams-doc2vec.gensim --dataset-path ../data/dataset-new/cleaned  --stemming
+python eval.py --model-name ../models/ams-fasttext.gensim --dataset-path ../data/dataset-new/cleaned  --stemming
 
 # stemming
-python eval.py --model-name ../models/ams-all-mpnet-base-v2-stem --dataset-path ../data/cleaned
-python eval.py --model-name ../models/ams-multi-qa-distilbert-cos-v1-stem --dataset-path ../data/cleaned
-python eval.py --model-name ../models/ams-multi-qa-MiniLM-L6-cos-v1-stem --dataset-path ../data/cleaned
-python eval.py --model-name ../models/ams-msmarco-distilbert-cos-v5-stem --dataset-path ../data/cleaned
-python eval.py --model-name ../models/ams-msmarco-MiniLM-L6-cos-v5-stem --dataset-path ../data/cleaned
-python eval.py --model-name ../models/ams-bow-stem.joblib --dataset-path ../data/cleaned
-python eval.py --model-name ../models/ams-tfidf-stem.joblib --dataset-path ../data/cleaned
-python eval.py --model-name ../models/ams-doc2vec-stem.gensim --dataset-path ../data/cleaned 
-python eval.py --model-name ../models/ams-fasttext-stem.gensim --dataset-path ../data/cleaned 
+python eval.py --model-name ../models/ams-all-mpnet-base-v2-stem --dataset-path ../data/dataset-new/cleaned
+python eval.py --model-name ../models/ams-multi-qa-distilbert-cos-v1-stem --dataset-path ../data/dataset-new/cleaned
+python eval.py --model-name ../models/ams-multi-qa-MiniLM-L6-cos-v1-stem --dataset-path ../data/dataset-new/cleaned
+python eval.py --model-name ../models/ams-msmarco-distilbert-cos-v5-stem --dataset-path ../data/dataset-new/cleaned
+python eval.py --model-name ../models/ams-msmarco-MiniLM-L6-cos-v5-stem --dataset-path ../data/dataset-new/cleaned
+python eval.py --model-name ../models/ams-melayubert-stem --dataset-path ../data/dataset-new/cleaned
+python eval.py --model-name ../models/ams-indobert-stem --dataset-path ../data/dataset-new/cleaned
+python eval.py --model-name ../models/ams-bow-stem.joblib --dataset-path ../data/dataset-new/cleaned
+python eval.py --model-name ../models/ams-tfidf-stem.joblib --dataset-path ../data/dataset-new/cleaned
+python eval.py --model-name ../models/ams-doc2vec-stem.gensim --dataset-path ../data/dataset-new/cleaned 
+python eval.py --model-name ../models/ams-fasttext-stem.gensim --dataset-path ../data/dataset-new/cleaned 
 
-python eval.py --model-name ../models/ams-all-mpnet-base-v2-stem --dataset-path ../data/cleaned --stemming
-python eval.py --model-name ../models/ams-multi-qa-distilbert-cos-v1-stem --dataset-path ../data/cleaned --stemming
-python eval.py --model-name ../models/ams-multi-qa-MiniLM-L6-cos-v1-stem --dataset-path ../data/cleaned --stemming
-python eval.py --model-name ../models/ams-msmarco-distilbert-cos-v5-stem --dataset-path ../data/cleaned --stemming
-python eval.py --model-name ../models/ams-msmarco-MiniLM-L6-cos-v5-stem --dataset-path ../data/cleaned --stemming
-python eval.py --model-name ../models/ams-bow-stem.joblib --dataset-path ../data/cleaned --stemming
-python eval.py --model-name ../models/ams-tfidf-stem.joblib --dataset-path ../data/cleaned --stemming
-python eval.py --model-name ../models/ams-doc2vec-stem.gensim --dataset-path ../data/cleaned --stemming
-python eval.py --model-name ../models/ams-fasttext-stem.gensim --dataset-path ../data/cleaned --stemming
+python eval.py --model-name ../models/ams-all-mpnet-base-v2-stem --dataset-path ../data/dataset-new/cleaned --stemming
+python eval.py --model-name ../models/ams-multi-qa-distilbert-cos-v1-stem --dataset-path ../data/dataset-new/cleaned --stemming
+python eval.py --model-name ../models/ams-multi-qa-MiniLM-L6-cos-v1-stem --dataset-path ../data/dataset-new/cleaned --stemming
+python eval.py --model-name ../models/ams-msmarco-distilbert-cos-v5-stem --dataset-path ../data/dataset-new/cleaned --stemming
+python eval.py --model-name ../models/ams-msmarco-MiniLM-L6-cos-v5-stem --dataset-path ../data/dataset-new/cleaned --stemming
+python eval.py --model-name ../models/ams-melayubert-stem --dataset-path ../data/dataset-new/cleaned --stemming
+python eval.py --model-name ../models/ams-indobert-stem --dataset-path ../data/dataset-new/cleaned --stemming
+python eval.py --model-name ../models/ams-bow-stem.joblib --dataset-path ../data/dataset-new/cleaned --stemming
+python eval.py --model-name ../models/ams-tfidf-stem.joblib --dataset-path ../data/dataset-new/cleaned --stemming
+python eval.py --model-name ../models/ams-doc2vec-stem.gensim --dataset-path ../data/dataset-new/cleaned --stemming
+python eval.py --model-name ../models/ams-fasttext-stem.gensim --dataset-path ../data/dataset-new/cleaned --stemming
+
+# perbandingan 50 dan 100 epochs
+python eval.py --model-name ../tmp/ams-indobert-stem-50 --dataset-path ../data/dataset-new/cleaned --stemming
+python eval.py --model-name ../tmp/ams-indobert-stem-100 --dataset-path ../data/dataset-new/cleaned --stemming
--- scripts/run-train.sh
+++ scripts/run-train.sh
@@ -1,22 +1,32 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-python train.py --model-name sentence-transformers/all-mpnet-base-v2 --dataset-path ../data/cleaned/triplet.jsonl --output-path ../models/ams-all-mpnet-base-v2
-python train.py --model-name sentence-transformers/multi-qa-distilbert-cos-v1 --dataset-path ../data/cleaned/triplet.jsonl --output-path ../models/ams-multi-qa-distilbert-cos-v1
-python train.py --model-name sentence-transformers/multi-qa-MiniLM-L6-cos-v1 --dataset-path ../data/cleaned/triplet.jsonl --output-path ../models/ams-multi-qa-MiniLM-L6-cos-v1
-python train.py --model-name sentence-transformers/msmarco-distilbert-cos-v5 --dataset-path ../data/cleaned/triplet.jsonl --output-path ../models/ams-msmarco-distilbert-cos-v5
-python train.py --model-name sentence-transformers/msmarco-MiniLM-L6-cos-v5 --dataset-path ../data/cleaned/triplet.jsonl --output-path ../models/ams-msmarco-MiniLM-L6-cos-v5
-python train.py --model-name ams-bow --dataset-path ../data/cleaned/triplet.jsonl --output-path ../models/ams-bow.joblib
-python train.py --model-name ams-tfidf --dataset-path ../data/cleaned/triplet.jsonl --output-path ../models/ams-tfidf.joblib
-python train.py --model-name ams-doc2vec --dataset-path ../data/cleaned/triplet.jsonl --output-path ../models/ams-doc2vec.gensim --epochs 100
-python train.py --model-name ams-fasttext --dataset-path ../data/cleaned/triplet.jsonl --output-path ../models/ams-fasttext.gensim --epochs 100
+# fine-tuning without stemming
+python train.py --model-name sentence-transformers/all-mpnet-base-v2 --dataset-path ../data/dataset-new/cleaned/triplet.jsonl --output-path ../models/ams-all-mpnet-base-v2
+python train.py --model-name sentence-transformers/multi-qa-distilbert-cos-v1 --dataset-path ../data/dataset-new/cleaned/triplet.jsonl --output-path ../models/ams-multi-qa-distilbert-cos-v1
+python train.py --model-name sentence-transformers/multi-qa-MiniLM-L6-cos-v1 --dataset-path ../data/dataset-new/cleaned/triplet.jsonl --output-path ../models/ams-multi-qa-MiniLM-L6-cos-v1
+python train.py --model-name sentence-transformers/msmarco-distilbert-cos-v5 --dataset-path ../data/dataset-new/cleaned/triplet.jsonl --output-path ../models/ams-msmarco-distilbert-cos-v5
+python train.py --model-name sentence-transformers/msmarco-MiniLM-L6-cos-v5 --dataset-path ../data/dataset-new/cleaned/triplet.jsonl --output-path ../models/ams-msmarco-MiniLM-L6-cos-v5
+python train.py --model-name StevenLimcorn/MelayuBERT --dataset-path ../data/dataset-new/cleaned/triplet.jsonl --output-path ../models/ams-melayubert
+python train.py --model-name indolem/indobert-base-uncased --dataset-path ../data/dataset-new/cleaned/triplet.jsonl --output-path ../models/ams-indobert
+python train.py --model-name ams-bow --dataset-path ../data/dataset-new/cleaned/triplet.jsonl --output-path ../models/ams-bow.joblib
+python train.py --model-name ams-tfidf --dataset-path ../data/dataset-new/cleaned/triplet.jsonl --output-path ../models/ams-tfidf.joblib
+python train.py --model-name ams-doc2vec --dataset-path ../data/dataset-new/cleaned/triplet.jsonl --output-path ../models/ams-doc2vec.gensim --epochs 100
+python train.py --model-name ams-fasttext --dataset-path ../data/dataset-new/cleaned/triplet.jsonl --output-path ../models/ams-fasttext.gensim --epochs 100
 
-python train.py --model-name sentence-transformers/all-mpnet-base-v2 --dataset-path ../data/cleaned/triplet.jsonl --output-path ../models/ams-all-mpnet-base-v2-stem --stemming
-python train.py --model-name sentence-transformers/multi-qa-distilbert-cos-v1 --dataset-path ../data/cleaned/triplet.jsonl --output-path ../models/ams-multi-qa-distilbert-cos-v1-stem --stemming
-python train.py --model-name sentence-transformers/multi-qa-MiniLM-L6-cos-v1 --dataset-path ../data/cleaned/triplet.jsonl --output-path ../models/ams-multi-qa-MiniLM-L6-cos-v1-stem --stemming
-python train.py --model-name sentence-transformers/msmarco-distilbert-cos-v5 --dataset-path ../data/cleaned/triplet.jsonl --output-path ../models/ams-msmarco-distilbert-cos-v5-stem --stemming
-python train.py --model-name sentence-transformers/msmarco-MiniLM-L6-cos-v5 --dataset-path ../data/cleaned/triplet.jsonl --output-path ../models/ams-msmarco-MiniLM-L6-cos-v5-stem --stemming
-python train.py --model-name ams-bow-stem --dataset-path ../data/cleaned/triplet.jsonl --output-path ../models/ams-bow-stem.joblib --stemming
-python train.py --model-name ams-tfidf-stem --dataset-path ../data/cleaned/triplet.jsonl --output-path ../models/ams-tfidf-stem.joblib --stemming
-python train.py --model-name ams-doc2vec-stem --dataset-path ../data/cleaned/triplet.jsonl --output-path ../models/ams-doc2vec-stem.gensim --stemming --epochs 100
-python train.py --model-name ams-fasttext-stem --dataset-path ../data/cleaned/triplet.jsonl --output-path ../models/ams-fasttext-stem.gensim --stemming --epochs 100
+# fine-tuning with stemming
+python train.py --model-name sentence-transformers/all-mpnet-base-v2 --dataset-path ../data/dataset-new/cleaned/triplet.jsonl --output-path ../models/ams-all-mpnet-base-v2-stem --stemming
+python train.py --model-name sentence-transformers/multi-qa-distilbert-cos-v1 --dataset-path ../data/dataset-new/cleaned/triplet.jsonl --output-path ../models/ams-multi-qa-distilbert-cos-v1-stem --stemming
+python train.py --model-name sentence-transformers/multi-qa-MiniLM-L6-cos-v1 --dataset-path ../data/dataset-new/cleaned/triplet.jsonl --output-path ../models/ams-multi-qa-MiniLM-L6-cos-v1-stem --stemming
+python train.py --model-name sentence-transformers/msmarco-distilbert-cos-v5 --dataset-path ../data/dataset-new/cleaned/triplet.jsonl --output-path ../models/ams-msmarco-distilbert-cos-v5-stem --stemming
+python train.py --model-name sentence-transformers/msmarco-MiniLM-L6-cos-v5 --dataset-path ../data/dataset-new/cleaned/triplet.jsonl --output-path ../models/ams-msmarco-MiniLM-L6-cos-v5-stem --stemming
+python train.py --model-name StevenLimcorn/MelayuBERT --dataset-path ../data/dataset-new/cleaned/triplet.jsonl --output-path ../models/ams-melayubert-stem --stemming
+python train.py --model-name indolem/indobert-base-uncased --dataset-path ../data/dataset-new/cleaned/triplet.jsonl --output-path ../models/ams-indobert-stem --stemming
+python train.py --model-name ams-bow-stem --dataset-path ../data/dataset-new/cleaned/triplet.jsonl --output-path ../models/ams-bow-stem.joblib --stemming
+python train.py --model-name ams-tfidf-stem --dataset-path ../data/dataset-new/cleaned/triplet.jsonl --output-path ../models/ams-tfidf-stem.joblib --stemming
+python train.py --model-name ams-doc2vec-stem --dataset-path ../data/dataset-new/cleaned/triplet.jsonl --output-path ../models/ams-doc2vec-stem.gensim --stemming --epochs 100
+python train.py --model-name ams-fasttext-stem --dataset-path ../data/dataset-new/cleaned/triplet.jsonl --output-path ../models/ams-fasttext-stem.gensim --stemming --epochs 100
+
+# fine-tuning with stemming extended
+python train.py --model-name indolem/indobert-base-uncased --dataset-path ../data/dataset-new/cleaned/triplet.jsonl --output-path ../models/ams-indobert-stem-50 --stemming --epochs 50
+python train.py --model-name indolem/indobert-base-uncased --dataset-path ../data/dataset-new/cleaned/triplet.jsonl --output-path ../models/ams-indobert-stem-100 --stemming --epochs 100
